e9469ca0e48017f4234715c3fee3ffc59fa87c35
Johnathan Tran <jtran304@gmail.com>
2022-02-11 12:25:03 -0600

    setupdb

--- models.py
+++ models.py
@@ -10,18 +10,20 @@ load_dotenv()
 #database_path = "postgres://{}/{}".format('localhost:5432', database_name)
 #database_path = "postgresql://{}:{}@{}/{}".format(os.environ["USER"], os.environ["PASSWORD"], 'localhost:5432', database_name)
 database_path = os.environ["DATABASE_URL"]
+
+
 db = SQLAlchemy()
 
-'''
+
 setup_db(app)
     binds a flask application and a SQLAlchemy service
-'''
+
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    db.create_all()
+    #db.create_all()
 
 '''
 Movie
