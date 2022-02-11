f7d5836d0f1ff67e16175c747877ec7d85bd8d48
Johnathan Tran <jtran304@gmail.com>
2022-02-10 19:19:55 -0600

    fixed folder structure

--- projects/02_trivia_api/starter/backend/config_template.py
+++
@@ -1,8 +0,0 @@
-# rename this file to "config.py"
-# production credentials, used in "models.py"
-user = "postgres"
-password = "password"
-
-# test credentials, used in "test_flaskr.py"
-test_user = "postgres"
-test_password = "password"
