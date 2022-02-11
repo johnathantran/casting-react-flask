b9c957179a32d5098b332ab34ab6afa7b49308af
Johnathan Tran <jtran304@gmail.com>
2022-02-11 12:46:27 -0600

    models.py

--- models.py
+++ models.py
@@ -11,12 +11,7 @@ load_dotenv()
 #database_path = "postgresql://{}:{}@{}/{}".format(os.environ["USER"], os.environ["PASSWORD"], 'localhost:5432', database_name)
 database_path = os.environ["DATABASE_URL"]
 
-
 db = SQLAlchemy()
-
-
-setup_db(app)
-    binds a flask application and a SQLAlchemy service
 
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
@@ -24,6 +19,8 @@ def setup_db(app, database_path=database_path):
     db.app = app
     db.init_app(app)
     db.create_all()
+
+#setup_db(app, database_path=database_path)
 
 '''
 Movie
